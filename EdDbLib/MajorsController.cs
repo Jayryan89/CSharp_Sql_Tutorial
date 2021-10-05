578c7f59a0770b9871e3d0aa5a6c3580c41ea4b7
Jason Ryan <jamariryan89@yahoo.com>
2021-10-05 08:28:13 -0500

    Add project files.

---
+++ EdDbLib/MajorsController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Data.SqlClient;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdDbLib
+{
+	public class MajorsController
+	{
+		private SqlConnection sqlConn { get; set; }
+
+		public MajorsController(Connection connection)
+		{
+			sqlConn = connection.SqlConnection;
+		}
+		public int Change(Major major)
+        {
+			var sql = " UPDATE Major Set " +
+					$" Code = '{major.Code}'," +
+					$"Description = {major.Description}'," +
+					$"MinSAT = {major.MinSAT} " +
+					$"Where Id = {major.Id}";
+			
+			var cmd = new SqlCommand(sql, sqlConn);
+			var rowsAffected = cmd.ExecuteNonQuery();
+			return rowsAffected;
+		}
+		public int Create(Major major)
+		{
+			var sql = " INSERT Major (Code, Description, MinSAT) " +
+					$" VALUES ('{major.Code}', '{major.Description}', {major.MinSAT}); ";
+			var cmd = new SqlCommand(sql, sqlConn);
+			var rowsAffected = cmd.ExecuteNonQuery();
+			return rowsAffected;
+		}
+		public List<Major> GetAll()
+		{
+
+			// connection opened fine!
+			var majors = new List<Major>();
+			var sql = "Select * from Major; ";
+			var cmd = new SqlCommand(sql, sqlConn);
+			var reader = cmd.ExecuteReader();
+			while (reader.Read())
+			{
+				var major = new Major()
+				{
+					Id = Convert.ToInt32(reader["Id"]),
+					Code = reader["Code"].ToString(),
+					Description = reader["Description"].ToString(),
+					MinSAT = Convert.ToInt32(reader["MinSAT"])
+				};
+				majors.Add(major);
+			}
+			reader.Close();
+
+			return majors;
+		}
+
+		public Major GetByPk(int Id)
+		{
+
+			var sql = $"Select * from Major where Id = {Id};";
+			var cmd = new SqlCommand(sql, sqlConn);
+			var reader = cmd.ExecuteReader();
+			if (!reader.HasRows)
+			{
+				reader.Close();
+				return null;
+			}
+			reader.Read();
+			var major = new Major()
+			{
+				Id = Convert.ToInt32(reader["Id"]),
+				Code = reader["Code"].ToString(),
+				Description = reader["Description"].ToString(),
+				MinSAT = Convert.ToInt32(reader["MinSAT"])
+			};
+
+			reader.Close();
+			return major;
+		}
+	}
+}
